171399e7125eea214fbd276036a6439d6208971e
Mike Pixel <86058062+mikepixeldev@users.noreply.github.com>
2025-03-21 07:23:40 +0100

    Reconstruction du header

--- WP-OhMyFood/app/public/wp-content/themes/oh-my-food/header.php
+++ WP-OhMyFood/app/public/wp-content/themes/oh-my-food/header.php
@@ -43,12 +43,65 @@
 			?>
 		</a>
 
-		<!-- Inclusion du fichier site-header.php situé dans template-parts/header/ -->
-		<!-- 
-			Ce fichier contient le code qui appelle les fonctions WordPress pour afficher le menu de navigation et le logo du header.
-			Le menu est le logo est modifiable dans l'administration de WordPress, dans personalisez > identité du site.
-		-->
-		<?php get_template_part('template-parts/header/site-header'); ?>
+		<!-- Nouveau header -->
+		<header>
+			<!-- Partie logo & menu du header -->
+			<div class="header-top">
+				<div class="logo-container">
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/logo-ohmyfood-header.svg" alt="Logo OhMyFood" class="site-logo">
+				</div>
+				<nav class="main-navigation">
+					<?php
+					wp_nav_menu(
+						array(
+							'theme_location' => 'primary',
+							'menu_class' => 'menu-wrapper',
+							'container_class' => 'primary-menu-container',
+							'items_wrap' => '<ul id="primary-menu-list" class="%2$s">%3$s</ul>',
+							'fallback_cb' => false,
+						)
+					);
+					?>
+				</nav>
+				<div class="header-profile">
+					<button class="hamburger-menu" aria-label="Menu">
+						<span></span>
+						<span></span>
+						<span></span>
+					</button>
+					<a href="#" class="profile-link">
+						<img src="/path/to/avatar.png" alt="Profil" class="profile-avatar">
+					</a>
+				</div>
+			</div>
+
+			<!-- Partie recherche du header -->
+			<div class="header-bottom">
+				<div class="search-bar">
+					<div class="search-field">
+						<span>Où ?</span>
+						<input type="text" placeholder="Rechercher un lieu">
+					</div>
+					<div class="search-field">
+						<span>Quand ?</span>
+						<input type="date">
+					</div>
+					<div class="search-field">
+						<span>Service</span>
+						<input type="text" placeholder="Midi ou soir ?">
+					</div>
+					<div class="search-field">
+						<span>Personnes</span>
+						<input type="number" placeholder="Combien de personnes ?">
+					</div>
+					<button class="search-button">
+						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+							<path d="M505 442.7l-99.7-99.7c28.4-35.3 45.4-80 45.4-128.8C450.7 98.9 351.8 0 225.3 0S0 98.9 0 225.3s98.9 225.3 225.3 225.3c48.8 0 93.5-17 128.8-45.4l99.7 99.7c4.7 4.7 10.9 7 17 7s12.3-2.3 17-7c9.4-9.4 9.4-24.6 .1-33.9zM225.3 384c-87.9 0-159.3-71.4-159.3-159.3S137.4 65.3 225.3 65.3 384.7 136.7 384.7 224.7 313.2 384 225.3 384z" />
+						</svg>
+					</button>
+				</div>
+			</div>
+		</header>
 
 		<!-- Conteneur principal du contenu du site -->
 		<div id="content" class="site-content">
